f0060a1e1762e8c5f1d0704dd333fc5ec36b2b67
yunweneric <yunweneric@gmail.com>
2020-06-30 12:05:08 +0100

    added nav bar and js typer

---
+++ typer.js
@@ -0,0 +1,23 @@
+const list = ["Developer", "Musician", "Leader", "Mentor", "Teacher", "Advocate"];
+let count = 0;
+let index = 0;
+let currenText = "";
+let letter = "";
+
+(function type(){
+    if (count === list.length){
+        count = 0;
+    }
+    currenText = list[count];
+    letter = currenText.slice(0, ++index);
+    content = document.querySelector(".typer1")
+    content.style.fontFamily
+    document.querySelector(".typer1").textContent = letter;
+    
+    if(letter.length ===currenText.length){
+        count++;
+        index = 0;
+    }
+    setTimeout(type, 250)
+
+})();
